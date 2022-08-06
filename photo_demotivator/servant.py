4a17c431a5a8da5fedb2442662f944aeb725b0c9
Vadim <106238244+vadushkin@users.noreply.github.com>
2022-08-06 06:30:59 +0300

    Update servant.py

--- photo_demotivator/servant.py
+++ photo_demotivator/servant.py
@@ -1,8 +1,10 @@
 import random
-from simpledemotivators import Demotivator
-import datetime
-import sqlite3
 import re
+import datetime
+
+from simpledemotivators import Demotivator
+import sqlite3
+
 
 date_base = sqlite3.connect("DiscordBase.sqlite3")
 cursor = date_base.cursor()
@@ -10,15 +12,20 @@ cursor = date_base.cursor()
 
 def photo_change(arguments, now):
     """Демотиватор"""
+    
     today = datetime.datetime.today()
+    
     if len(arguments) == 1 and arguments != ['']:
         dem = Demotivator(arguments[0])  # 2 строчки
+        
     elif len(arguments) > 1:
         dem = Demotivator(arguments[0], arguments[1])
+        
     else:
         sqlite_select_query = """SELECT * from messages WHERE links = 2 ORDER BY RANDOM() LIMIT 1;"""
         cursor.execute(sqlite_select_query)
         records = cursor.fetchone()
+        
         if random.randint(1, 5) in [1, 2, 4, 5]:
             sqlite_select_query = """SELECT * from messages WHERE links = 2 ORDER BY RANDOM() LIMIT 1;"""
             cursor.execute(sqlite_select_query)
@@ -26,16 +33,19 @@ def photo_change(arguments, now):
             dem = Demotivator(str(records[1]), str(records2[1]))
         else:
             dem = Demotivator(str(records[1]))
-    v = 'photo_demotivator\\' + today.strftime("%Y-%m-%d-%H.%M.%S").replace('.', '-') + '.jpeg'
-    dem.create(now, result_filename=v)
-    return v
+            
+    name_file = 'photo_demotivator\\' + today.strftime("%Y-%m-%d-%H.%M.%S").replace('.', '-') + '.jpeg'
+    dem.create(now, result_filename=name_file)
+    return name_file
 
 
 def looking_for_a_link():
     """Ищет ссылку на фотографию, чтобы подставить в демотиватора"""
+    
     cursor.execute("""SELECT COUNT(*) FROM messages""")
     records = cursor.fetchone()
-    for i in range(records[0]):
+    
+    for number in range(records[0]):
 
         sqlite_select_query = """SELECT * from messages WHERE links = 1 ORDER BY RANDOM() LIMIT 1;"""
         cursor.execute(sqlite_select_query)
